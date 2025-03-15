9d664310a14e882a57256549b0e6b1d988be3378
Zainab Chaudary <zainabchaudary35@gmail.com>
2025-03-15 21:08:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function appendValue(value) {
+    document.getElementById("display").value += value;
+}
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+function calculateResult() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch {
+        alert("Invalid Expression");
+    }
+}
